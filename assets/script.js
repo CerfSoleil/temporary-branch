c9dbcf6addad0c6e14ee71a7c0b10c128291fd7c
CerfSoleil <robedj12@gmail.com>
2024-11-08 01:37:50 -0600

    Script.js Update

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-let gameCanvas;
-let ctx;
+const gameCanvas = document.getElementById("playArea");
+const ctx = gameCanvas.getContext("2d");
 
 let blockSize = 10;
 let rowSize = 40;
@@ -21,23 +21,18 @@ let gameOver = false;
 
 //This will need to activate by the play button.
 window.onload = function () {
-    gameCanvas = document.getElementById("playArea");
+    //This sets the playing field to the canvas size.
     gameCanvas.height = rowSize * blockSize;
     gameCanvas.width = colSize * blockSize;
-    ctx = gameCanvas.getContext("2d");
 
     placeFood();
     document.addEventListener("keydown", changeDirection);
     // This begins the game and sets its speed
-    setInterval(game, 1000 / 10);
+    setInterval(game, 100);
 }
 
 function game() {
-    if (gameOver) {
-        return;
-    }
-
-    // The game's background color
+     // The game's background color
     ctx.fillStyle = '#77A7F7';
     ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 
@@ -75,6 +70,7 @@ function game() {
         if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) { 
             gameOver = true;
             alert("Game Over");
+            return;
         }
     }
 
@@ -82,6 +78,7 @@ function game() {
     if (snakeX < 0 || snakeX > colSize * blockSize || snakeY < 0 || snakeY > rowSize * blockSize) {         
         gameOver = true;
         alert("Game Over");
+        return;
     }
 }
 
